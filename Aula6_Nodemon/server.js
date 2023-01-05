bdaad06b82bd87d5de3022db2dd70d7bb2a89c88
kelvyn da costa silva <kelvyncssilva@gmail.com>
2023-01-05 10:25:43 -0300

    Foi visto nessa aula sobre o nodemon, com fazer a instalação dele no node utilizando o npm install nodemon.

---
+++ Aula6_Nodemon/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, resp) => {
+    resp.send(`
+    <form action="/" method="POST">
+    Nome do cliente: <input type="text" name"nome">
+    <button>Ola</button>
+    </form>
+    `);
+});
+
+app.post('/', (req, res) => {
+    res.send('Recebi o formulario');
+});
+
+app.get('/contato', (req, resp) => {
+    resp.send('Obrigado por entrar em contato com a gente');
+});
+
+app.listen(3000, () => {
+    console.log('http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+});
